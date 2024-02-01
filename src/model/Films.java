8a867849f41322d162dca2103024f7f2bfa7b25c
Ekaitz B <ekaitz.blancano@elorrieta-errekamari.com>
2024-02-01 12:06:17 +0100

    Film eta Tickets klaseak editatuta

--- src/model/Films.java
+++ src/model/Films.java
@@ -1,10 +1,13 @@
 package model;
 
+import java.util.Objects;
+
 import model.methods.Converters;
 
 public class Films {
 	//VARIABLES
 	private int film_number;
+	private String film_name;
 	private double film_duration;
 	private double film_price;
 	private String film_genre;
@@ -14,15 +17,17 @@ public class Films {
 		
 	}
 	
-	public Films(int film_number, double film_duration, double film_price, String film_genre) {
+	public Films(int film_number,String film_name, double film_duration, double film_price, String film_genre) {
+		this.film_number = film_number;
 		this.film_number = film_number;
 		this.film_duration = film_duration;
 		this.film_price = film_price;
 		this.film_genre = film_genre;
 	}
 	
-	public Films(String film_number, String film_duration, String film_price, String film_genre) {
+	public Films(String film_number,String film_name, String film_duration, String film_price, String film_genre) {
 		this.film_number = Converters.stringToInt(film_number);
+		this.film_name = film_name;
 		this.film_duration = Converters.stringToDouble(film_duration);
 		this.film_price = Converters.stringToDouble(film_price);
 		this.film_genre = film_genre;
@@ -30,21 +35,24 @@ public class Films {
 	
 	@Override
 	public String toString() {
-		return "Films [film_number=" + film_number + ", film_duration=" + film_duration + ", film_price=" + film_price
-				+ ", film_genre=" + film_genre + "]";
+		return "Films [film_number=" + film_number + ", film_name=" + film_name + ", film_duration=" + film_duration
+				+ ", film_price=" + film_price + ", film_genre=" + film_genre + "]";
 	}
 
-	public boolean equals(Films object) {
-        if (this.film_number  == object.film_number &&
-            this.film_duration == object.film_duration &&
-            this.film_price == object.film_price &&
-            this.film_genre.equals(object.film_genre)) {
-            
-            return true;
-        }
-        
-        return false;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Films other = (Films) obj;
+		return Double.doubleToLongBits(film_duration) == Double.doubleToLongBits(other.film_duration)
+				&& Objects.equals(film_genre, other.film_genre) && Objects.equals(film_name, other.film_name)
+				&& film_number == other.film_number
+				&& Double.doubleToLongBits(film_price) == Double.doubleToLongBits(other.film_price);
+	}	
 
 	// GETTERS & SETTERS
 	public int getFilm_number() {
@@ -52,6 +60,12 @@ public class Films {
 	}
 	public void setFilm_number(int film_number) {
 		this.film_number = film_number;
+	}
+	public String getFilm_name() {
+		return film_name;
+	}
+	public void setFilm_name(String film_name) {
+		this.film_name = film_name;
 	}
 	public double getFilm_duration() {
 		return film_duration;
--- src/model/Tickets.java
+++ src/model/Tickets.java
@@ -1,84 +1,110 @@
 package model;
 
+import java.util.Objects;
+
 import model.methods.Converters;
 
 public class Tickets {
 	// VARIABLES
 	private int ticket_number;
-	private String date;
-	private int film;
-	private int room;
+	private String cinema_name;
+	private String room_name;
+	private String film_name;
 	private double price;
-	
+	private String session_date;
+
 	// CONSTRUCTORS
 	public Tickets() {
-		
-	}
-	
-	public Tickets(int ticket_number, String date, int film, int room, double price) {
-		this.ticket_number = ticket_number;
-		this.date = date;
-		this.film = film;
-		this.room = room;
-		this.price = price;
-	}
-	
-	public Tickets(String ticket_number, String date, String film, String room, String price) {
-		this.ticket_number = Converters.stringToInt(ticket_number);
-		this.date = date;
-		this.film = Converters.stringToInt(film);
-		this.room = Converters.stringToInt(room);
-		this.price = Converters.stringToDouble(price);
-	}
-	
-	@Override
-	public String toString() {
-		return "Tickets [ticket_number=" + ticket_number + ", date=" + date + ", film=" + film + ", room=" + room
-				+ ", price=" + price + "]";
+
 	}
 
-	public boolean equals(Tickets object) {
-        if (this.ticket_number == object.ticket_number &&
-            this.date.equals(object.date) &&
-            this.film == object.film &&
-            this.room == object.room &&
-            this.price == object.price) {
-            return true;
-        }
-        return false;
-    }
-	
-	// SETTERS & GETTERS
+	public Tickets(int ticket_number, String cinema_name, String room_name, String film_name, double price,
+			String session_date) {
+		this.ticket_number = ticket_number;
+		this.cinema_name = cinema_name;
+		this.room_name = room_name;
+		this.film_name = film_name;
+		this.price = price;
+		this.session_date = session_date;
+	}
+
+	public Tickets(String ticket_number, String cinema_name, String room_name, String film_name, String price,
+			String session_date) {
+		this.ticket_number = Converters.stringToInt(ticket_number);
+		this.cinema_name = cinema_name;
+		this.room_name = room_name;
+		this.film_name = film_name;
+		this.price = Converters.stringToDouble(price);
+		this.session_date = session_date;
+	}
+
+	@Override
+	public String toString() {
+		return "Tickets [ticket_number=" + ticket_number + ", cinema_name=" + cinema_name + ", room_name=" + room_name
+				+ ", film_name=" + film_name + ", price=" + price + ", session_date=" + session_date + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tickets other = (Tickets) obj;
+		return Objects.equals(cinema_name, other.cinema_name) && Objects.equals(film_name, other.film_name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& Objects.equals(room_name, other.room_name) && Objects.equals(session_date, other.session_date)
+				&& ticket_number == other.ticket_number;
+	}
+
 	public int getTicket_number() {
 		return ticket_number;
 	}
+
 	public void setTicket_number(int ticket_number) {
 		this.ticket_number = ticket_number;
 	}
-	public String getDate() {
-		return date;
+
+	public String getCinema_name() {
+		return cinema_name;
 	}
-	public void setDate(String date) {
-		this.date = date;
+
+	public void setCinema_name(String cinema_name) {
+		this.cinema_name = cinema_name;
 	}
-	public int getFilm() {
-		return film;
+
+	public String getRoom_name() {
+		return room_name;
 	}
-	public void setFilm(int film) {
-		this.film = film;
+
+	public void setRoom_name(String room_name) {
+		this.room_name = room_name;
 	}
-	public int getRoom() {
-		return room;
+
+	public String getFilm_name() {
+		return film_name;
 	}
-	public void setRoom(int room) {
-		this.room = room;
+
+	public void setFilm_name(String film_name) {
+		this.film_name = film_name;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
-	
-	// METHODS
+
+	public String getSession_date() {
+		return session_date;
+	}
+
+	public void setSession_date(String session_date) {
+		this.session_date = session_date;
+	}
+
 }
