1c8ff640aedcd29a088e47084b96a67bd0454591
Markel <markel.romango@elorrieta-errekamari.com>
2024-02-08 12:21:50 +0100

    Lehen commit Sprint2

--- src/controller/APP.java
+++ src/controller/APP.java
@@ -1,15 +1,11 @@
 package controller;
 
-import java.util.ArrayList;
 
-import org.mindrot.jbcrypt.BCrypt;
 
-import model.*;
-import view.*;
 
 public class APP {
 
 	public static void main(String[] args) {
-
+		System.out.println("kaixo");
 	}
 }
