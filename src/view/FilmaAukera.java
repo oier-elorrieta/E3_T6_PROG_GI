330a248433450a1369db6971ae122ab6ea5fa7bb
JonV <jon.vazquezfernandez@elorrieta-errekamari.com>
2024-02-16 12:46:37 +0100

    FilmaAukera-ren view-ean filmen lista polita ipini

--- src/view/FilmaAukera.java
+++ src/view/FilmaAukera.java
@@ -3,6 +3,9 @@ package view;
 import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.GraphicsConfiguration;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -16,80 +19,104 @@ import model.metodoak.*;
 import model.sql.*;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import java.awt.CardLayout;
+import javax.swing.JScrollPane;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import java.awt.FlowLayout;
 
 public class FilmaAukera extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					FilmaAukera frame = new FilmaAukera();
-					frame.setVisible(false);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    FilmaAukera frame = new FilmaAukera();
+                    frame.setVisible(false);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public FilmaAukera() {
-		System.out.println();
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 250, 1182, 683);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		
-		JButton btnLogin = View_metodoak.btn_login();
-		
-		JButton btnJarraitu = View_metodoak.btn_jarraitu();
-		
-		JButton btnAmaiera = View_metodoak.btn_amaiera();
-		
-		ButtonGroup bg = new ButtonGroup();
-		
-		JLabel lblFilmLista = new JLabel("FILMEN LISTA");
-		lblFilmLista.setHorizontalAlignment(SwingConstants.CENTER);
-		lblFilmLista.setBounds(310, 11, 452, 58);
-		lblFilmLista.setFont(new Font("Source Sans Pro Black", Font.BOLD, 45));
-		contentPane.add(lblFilmLista);
-		
-		JLabel lblAukeratuFilma = new JLabel("Aukeratu zinema bat:");
-		lblAukeratuFilma.setBounds(79, 70, 239, 25);
-		lblAukeratuFilma.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAukeratuFilma.setVerticalAlignment(SwingConstants.TOP);
-		lblAukeratuFilma.setFont(new Font("SansSerif", Font.BOLD, 18));
-		contentPane.add(lblAukeratuFilma);
-		
-		Filma[] filmaux = new Filma[KontsultakSQL.kont_filmak];
-		filmaux = KontsultakSQL.karteldegiaSortuta.getFilmaList();
-		int bound = 100;
-		
-		for (int i = 0; i < KontsultakSQL.kont_filmak; i++) {
-			JRadioButton rdbtnFilma = new JRadioButton(filmaux[i].getFilma_izena());
-			rdbtnFilma.setBounds(bound, 300 , 200, 54);
-			rdbtnFilma.setHorizontalAlignment(SwingConstants.CENTER);
-			rdbtnFilma.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-			rdbtnFilma.setFocusPainted(false);
-			contentPane.setLayout(null);
-			contentPane.add(rdbtnFilma);
-			bound = bound + 165;
-			bg.add(rdbtnFilma);
-		}
-		
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.add(btnLogin);
-		contentPane.add(btnAmaiera);
-		contentPane.add(btnJarraitu);
-		
-	}
+    /**
+     * Create the frame.
+     * @param zinemaAukera 
+     */
+    
+    public FilmaAukera() {
+        System.out.println();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(400, 250, 1182, 683);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        
+        JPanel panelFilmak = new JPanel();
+        panelFilmak.setBorder(new CompoundBorder());
+        panelFilmak.setBounds(79, 125, 900, 410);
+        contentPane.add(panelFilmak);
+        panelFilmak.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+    
+        
+        JButton btnLogin = View_metodoak.btn_login();
+        
+        JButton btnJarraitu = View_metodoak.btn_jarraitu();
+        
+        JButton btnAmaiera = View_metodoak.btn_amaiera();
+        
+        JLabel lblFilmLista = new JLabel("FILMEN LISTA");
+        lblFilmLista.setHorizontalAlignment(SwingConstants.CENTER);
+        lblFilmLista.setBounds(310, 11, 452, 58);
+        lblFilmLista.setFont(new Font("Source Sans Pro Black", Font.BOLD, 45));
+        contentPane.add(lblFilmLista);
+        
+        JLabel lblAukeratuFilma = new JLabel("Aukeratu filma bat:");
+        lblAukeratuFilma.setBounds(79, 70, 239, 25);
+        lblAukeratuFilma.setHorizontalAlignment(SwingConstants.CENTER);
+        lblAukeratuFilma.setVerticalAlignment(SwingConstants.TOP);
+        lblAukeratuFilma.setFont(new Font("SansSerif", Font.BOLD, 18));
+        contentPane.add(lblAukeratuFilma);
+
+        ButtonGroup bg = new ButtonGroup();
+        int bound = 100;
+        
+        panelFilmak.setLayout(new GridLayout(0, 2));
+        
+        Saioa[] saioaAukera = ZinemaAukera.zinemaAukera.getSaioak();
+        for (int i = 0; i < saioaAukera.length;i++) {
+            JRadioButton rdbtnZinema = new JRadioButton(saioaAukera[i].getFilma().getFilma_izena() + " // " + saioaAukera[i].getFilma().getFilma_iraupena()+ " min.");
+            rdbtnZinema.setActionCommand(String.valueOf(i));
+            rdbtnZinema.setBounds(bound, 300 , 200, 54);
+            rdbtnZinema.setHorizontalAlignment(SwingConstants.CENTER);
+            rdbtnZinema.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+            rdbtnZinema.setFocusPainted(false);
+            panelFilmak.add(rdbtnZinema);
+            bound = bound + 210;
+            bg.add(rdbtnZinema);
+        }
+        
+       
+        
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        contentPane.add(btnLogin);
+        contentPane.add(btnAmaiera);
+        contentPane.add(btnJarraitu);
+        
+      
+        btnAmaiera.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                dispose();
+            }
+        });
+    }
 }
--- src/view/ZinemaAukera.java
+++ src/view/ZinemaAukera.java
@@ -17,92 +17,91 @@ import java.awt.event.MouseEvent;
 import java.util.Arrays;
 
 public class ZinemaAukera extends JFrame {
-	private static final long serialVersionUID = 1L;
-	public static final Zinema[] zinemaAux = KontsultakSQL.zinemaKudeatzailea.getZinemaList(); 
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private static final Zinema[] zinemaAux = KontsultakSQL.zinemaKudeatzailea.getZinemaList();
+    public static Zinema zinemaAukera;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ZinemaAukera frame = new ZinemaAukera();
-					frame.setVisible(false);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ZinemaAukera frame = new ZinemaAukera();
+                    frame.setVisible(false);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-	/**
-	 * Create the frame.
-	 */
-	public ZinemaAukera() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(400, 250, 1182, 683);
-		contentPane = new JPanel();
+    /**
+     * Create the frame.
+     */
+    public ZinemaAukera() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(400, 250, 1182, 683);
+        contentPane = new JPanel();
 
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblZinemaLista = new JLabel("ZINEMA LISTA");
-		lblZinemaLista.setFont(new Font("Source Sans Pro Black", Font.BOLD, 45));
-		lblZinemaLista.setBounds(304, 11, 452, 58);
-		lblZinemaLista.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		JLabel lblAukeratu = new JLabel("Aukeratu zinema bat:");
-		lblAukeratu.setBounds(79, 70, 239, 25);
-		lblAukeratu.setHorizontalAlignment(SwingConstants.CENTER);
-		lblAukeratu.setVerticalAlignment(SwingConstants.TOP);
-		lblAukeratu.setFont(new Font("SansSerif", Font.BOLD, 18));
-		
-		JButton btnJarraitu = View_metodoak.btn_jarraitu();
-		
-		JButton btnAmaiera = View_metodoak.btn_amaiera();
-		
-		JButton btnLogin = View_metodoak.btn_login();
-		
-		ButtonGroup bg = new ButtonGroup();
-		
-		int bound = 100;
-		
-		
-		for (int i = 0; i < KontsultakSQL.kont_zinemak; i++) {
-			JRadioButton rdbtnZinema = new JRadioButton(zinemaAux[i].getZinema_izena());
-			rdbtnZinema.setActionCommand(String.valueOf(i));
-			rdbtnZinema.setBounds(bound, 300 , 200, 54);
-			rdbtnZinema.setHorizontalAlignment(SwingConstants.CENTER);
-			rdbtnZinema.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-			rdbtnZinema.setFocusPainted(false);
-			contentPane.setLayout(null);
-			contentPane.add(rdbtnZinema);
-			bound = bound + 165;
-			bg.add(rdbtnZinema);
-		}
-		contentPane.add(lblZinemaLista);
-		contentPane.add(btnLogin);
-		contentPane.add(lblAukeratu);
-		contentPane.add(btnAmaiera);
-		contentPane.add(btnJarraitu);
-		
-		btnJarraitu.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				int aukera = 0;
-				aukera = Integer.parseInt(bg.getSelection().getActionCommand());
-				Zinema zinemaAukera = zinemaAux[aukera];
-				
-				
-				dispose();
-				FilmaAukera filmaAukera = new FilmaAukera();
-				filmaAukera.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-				filmaAukera.setVisible(true);
-			}
-		});
-	}
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        
+        JLabel lblZinemaLista = new JLabel("ZINEMA LISTA");
+        lblZinemaLista.setFont(new Font("Source Sans Pro Black", Font.BOLD, 45));
+        lblZinemaLista.setBounds(304, 11, 452, 58);
+        lblZinemaLista.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        JLabel lblAukeratu = new JLabel("Aukeratu zinema bat:");
+        lblAukeratu.setBounds(79, 70, 239, 25);
+        lblAukeratu.setHorizontalAlignment(SwingConstants.CENTER);
+        lblAukeratu.setVerticalAlignment(SwingConstants.TOP);
+        lblAukeratu.setFont(new Font("SansSerif", Font.BOLD, 18));
+        
+        JButton btnJarraitu = View_metodoak.btn_jarraitu();
+        
+        JButton btnAmaiera = View_metodoak.btn_amaiera();
+        
+        JButton btnLogin = View_metodoak.btn_login();
+        
+        ButtonGroup bg = new ButtonGroup();
+        
+        int bound = 100;
+        
+        
+        for (int i = 0; i < KontsultakSQL.kont_zinemak; i++) {
+            JRadioButton rdbtnZinema = new JRadioButton(zinemaAux[i].getZinema_izena());
+            rdbtnZinema.setActionCommand(String.valueOf(i));
+            rdbtnZinema.setBounds(bound, 285 , 200, 54);
+            rdbtnZinema.setHorizontalAlignment(SwingConstants.CENTER);
+            rdbtnZinema.setFont(new Font("Segoe UI", Font.PLAIN, 25));
+            rdbtnZinema.setFocusPainted(false);
+            contentPane.setLayout(null);
+            contentPane.add(rdbtnZinema);
+            bound = bound + 210;
+            bg.add(rdbtnZinema);
+        }
+        contentPane.add(lblZinemaLista);
+        contentPane.add(btnLogin);
+        contentPane.add(lblAukeratu);
+        contentPane.add(btnAmaiera);
+        contentPane.add(btnJarraitu);
+        
+        btnJarraitu.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int aukera = 0;
+                aukera = Integer.parseInt(bg.getSelection().getActionCommand());
+                zinemaAukera = zinemaAux[aukera];
+                dispose();
+                FilmaAukera filmaAukera = new FilmaAukera();
+                filmaAukera.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+                filmaAukera.setVisible(true);
+            }
+        });
+    }
 }
